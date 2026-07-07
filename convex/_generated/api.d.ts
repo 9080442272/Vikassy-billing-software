0359587e02cd76936e2040f3d83cab9d9c73e368
vikashini <vikashini@vikashinis-MacBook-Air.local>
2026-07-07 11:11:18 +0530

    style: fix password fields size mapping by adding input[type=password] selectors

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -1,8 +1,13 @@
-import type {
-  ApiFromModules,
-  FilterApi,
-  FunctionReference,
-} from "convex/server";
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
 import type * as bills from "../bills.js";
 import type * as ceoActivities from "../ceoActivities.js";
 import type * as clients from "../clients.js";
@@ -10,6 +15,12 @@ import type * as employees from "../employees.js";
 import type * as fabrics from "../fabrics.js";
 import type * as stitching from "../stitching.js";
 import type * as users from "../users.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
 
 declare const fullApi: ApiFromModules<{
   bills: typeof bills;
@@ -20,7 +31,31 @@ declare const fullApi: ApiFromModules<{
   stitching: typeof stitching;
   users: typeof users;
 }>;
+
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
 >;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
